432bc496293f96554a65d8b74b6b620ea52a2053
Spicyhuen <hyuen.yutico@gmail.com>
2023-03-24 16:40:14 +0000

    minor fix

---
+++ scripts/shop.js
@@ -0,0 +1,23 @@
+const snorkling = document.getElementById("snorkling-btn");
+const hiking = document.getElementById("hiking-btn");
+const fishing = document.getElementById("fishing-btn");
+const snowboarding = document.getElementById("snowboarding-btn");
+
+snorkling.addEventListener("click", function() {
+  window.alert("Snorkling selected");
+});
+
+
+hiking.addEventListener("click", function() {
+    window.alert("Hiking selected");
+  });
+
+  
+fishing.addEventListener("click", function() {
+    window.alert("Fishing selected");
+});
+
+
+snowboarding.addEventListener("click", function() {
+    window.alert("Snowboarding seleceted");
+});
