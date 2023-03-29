768d24f3790c506dfd97f0640f60ee17374b4693
DetorGN <deondrematthews2002@gmail.com>
2023-03-29 11:32:43 +0100

    Trying to add progress (hope this works)

--- detor_branch/scripts/gallery.js
+++ detor_branch/scripts/gallery.js
@@ -3,6 +3,29 @@ var center = document.querySelector('.center');
 var centerImg = document.querySelector('.center img');
 var centerDesc = document.querySelector('.center p');
 
+document.addEventListener("click", e => {
+    let handle
+    if (e.target.matches(".handle")) {
+      handle = e.target
+    } else {
+        handle = e.target.closest(".handle")
+    }
+    if (handle != null) onHandlePress(handle)
+})
+
+function onHandlePress(handle) {
+    const content = handle.closest(".content").querySelector(".images")
+    const contentIndex = parseInt(
+        getComputedStyle(content).getPropertyValue("--images-index"))
+    if (handle.classList.contains("left-handle")){
+        content.style.setProperty("--images-index", contentIndex - 1)
+    }
+    if (handle.classList.contains("right-handle")){
+        content.style.setProperty("--images-index", contentIndex + 1)
+    }
+}
+
+
 images.forEach(function (image) {
     image.addEventListener('mouseover', function () {
         centerImg.src = this.src;
