72d8447924030c4762b24fb2fea1f58a058e3ca3
DetorGN <deondrematthews2002@gmail.com>
2023-03-29 21:48:36 +0100

    Nearly finished Gallery, nearly done Page Editor

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,46 +1,40 @@
-var images = document.querySelectorAll('.gallery img');
-var center = document.querySelector('.center');
-var centerImg = document.querySelector('.center img');
-var centerDesc = document.querySelector('.center p');
-
 document.addEventListener("click", e => {
-    let handle
+    let handle;
     if (e.target.matches(".handle")) {
-      handle = e.target
+      handle = e.target;
     } else {
-        handle = e.target.closest(".handle")
+      handle = e.target.closest(".handle");
     }
-    if (handle != null) onHandlePress(handle)
-})
-
-function onHandlePress(handle) {
-    const content = handle.closest(".content").querySelector(".images")
+    if (handle != null) onHandlePress(handle);
+  });
+  
+  function onHandlePress(handle) {
+    const content = handle.closest(".content").querySelector(".images");
     const contentIndex = parseInt(
-        getComputedStyle(content).getPropertyValue("--images-index"))
-    if (handle.classList.contains("left-handle")){
-        content.style.setProperty("--images-index", contentIndex - 1)
+      getComputedStyle(content).getPropertyValue("--images-index")
+    );
+    if (handle.classList.contains("left-handle")) {
+      content.style.setProperty("--images-index", contentIndex - 1);
     }
-    if (handle.classList.contains("right-handle")){
-        content.style.setProperty("--images-index", contentIndex + 1)
+    if (handle.classList.contains("right-handle")) {
+      content.style.setProperty("--images-index", contentIndex + 1);
     }
-}
-
-
-images.forEach(function (image) {
-    image.addEventListener('mouseover', function () {
-        centerImg.src = this.src;
-        var desc = this.parentElement.querySelector('.description');
-        if (desc) {
-            centerDesc.textContent = desc.textContent;
-        } else {
-            centerDesc.textContent = "";
-        }
-        center.style.display = 'block';
-        this.classList.add('hovered');
+  }
+  
+  const images = document.querySelectorAll('.images img');
+  const imgInside = document.querySelector('.img-inside img');
+  
+  images.forEach((image) => {
+    image.addEventListener('click', () => {
+      const caption = document.querySelector('.correct-placement');
+      caption.innerHTML = image.getAttribute('data-caption');
+      caption.classList.add('active');
     });
-
-    image.addEventListener('mouseout', function () {
-        center.style.display = 'none';
-        this.classList.remove('hovered');
+  });
+  
+  images.forEach(image => {
+    image.addEventListener('click', () => {
+      imgInside.src = image.src;
     });
-});
+  });
+  
