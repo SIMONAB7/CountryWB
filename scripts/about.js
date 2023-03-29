b6f955fd7e8941db72d8ef44293b5e5a4d13145e
Daniel Contreras <ContrerasWilsonDD@cardiff.ac.uk>
2023-03-29 20:37:08 +0100

    started additional content, fixed student details

--- scripts/about.js
+++ scripts/about.js
@@ -1,37 +1,33 @@
 const windows = document.querySelector(".window");
 const student = document.querySelector(".students");
-const stuDiv = student.querySelector("div");
-const activeStu = document.querySelector(".activeStudent");
-const stu1 = document.querySelector("#student_1");
-const stu2 = document.querySelector("#student_2");
-const stuImg = stu1.querySelector("#image1");
-const activeStuTxt = activeStu.querySelector(".text");
 
-stu1.addEventListener("mouseover", autoOver);
-stu1.addEventListener("mouseout", autoOut);
+var hovers = document.getElementsByClassName("student");
+for (var i=0; i<hovers.length; i++) {
+    hovers[i].addEventListener("mouseover", autoOver);
+    hovers[i].addEventListener("mouseout", autoOut);
+}
 
 function autoOver() {
-    // this.querySelector("#image1").style.height = "250px";
-    stu1.style.height = "400px";
-    stu1.style.transition = "0.3s ease";
-    
+    this.style.transform = "scale(1.1)"
+    this.style.height = "410px";
+    this.style.transition = "0.3s ease";
+    this.style.marginBottom = "-90px";
+
+    for (var i=0;i<hovers.length;i++){
+        if (hovers[i] != this) {
+            hovers[i].style.opacity = "0.7";
+        }
+    }
 }
 
 function autoOut() {
+    this.style.transform = "scale(1.0)"
     this.style.height = "320px";
+    this.style.marginBottom = "0";
+
+    for (var i=0;i<hovers.length;i++){
+        if (hovers[i] != this) {
+            hovers[i].style.opacity = "1";
+        }
+    }
 }
-
-
-// function getStudent(student){
-//     const stuImg = student.querySelector("#image1").getAttribute("src");
-//     // const stuImgAlt = student.getElementById("image1").getAttribute("alt");
-//     const name = student.querySelector(".student_btn").innerHTML;
-    
-
-//     let imgHTML = '<img src="' + stuImg + '" alt="hello">';
-//     activeStuImg.innerHTML = imgHTML;
-//     activeStuImg.style.visibility = 'visible';
-
-//     activeStuTxt.innerHTML = name;
-//     activeStuTxt.style.visibility = 'visible';
-// }
