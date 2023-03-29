bbe29d23c16ac5fa8f4aa93747a01deaee50c86c
SimonaB <bosilkovasimona@gmail.com>
2023-03-29 11:58:22 +0100

    minor changes

---
+++ scripts/sitemap.js
@@ -0,0 +1,34 @@
+document.getElementById("home").
+addEventListener("click", function() {
+document.location.href="index.html";
+});
+
+document.getElementById("comments").
+addEventListener("click", function() {
+document.location.href="comments.html";
+ });
+
+document.getElementById("shop").
+addEventListener("click", function() {
+document.location.href="shop.html";
+});
+
+document.getElementById("sitemap").
+addEventListener("click", function() {
+document.location.href="sitemap.html";
+ });
+
+document.getElementById("quiz").
+addEventListener("click", function() {
+document.location.href="quiz.html";
+});
+
+document.getElementById("gallery").
+addEventListener("click", function() {
+document.location.href="gallery.html";
+});
+
+document.getElementById("about").
+addEventListener("click", function() {
+document.location.href="about.html";
+});
