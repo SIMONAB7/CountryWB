23f01724bcff12a66de3f3d76417d93396ffe410
Spicy Ramen <56414768+yhuen24@users.noreply.github.com>
2023-03-31 18:20:50 +0100

    remmove commnet

--- scripts/presentation.js
+++ scripts/presentation.js
@@ -1,6 +1,6 @@
 setTimeout(() => {
-    document.body.classList.add("fadeout"); // add the "fadeout" class to the body
+    document.body.classList.add("fadeout"); 
     setTimeout(() => {
-      window.location.href = "index.html"; // redirect to the new page after a short delay
-    }, 2000); // adjust the delay time (in milliseconds) as needed
-  }, 3000); // wait 5000 milliseconds (or 5 seconds) before triggering the redirect
+      window.location.href = "index.html"; 
+    }, 2000); 
+  }, 3000); 
